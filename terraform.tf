3401edc347a09ca19d8eccf1ca687914b3dd9c20
grolfskin <gkutsarov123@gmail.com>
2024-12-15 20:36:23 +0200

    Static kubeconfig setup

--- terraform.tf
+++ terraform.tf
@@ -19,21 +19,13 @@ terraform {
 
 provider "aws" {
   region = var.region
-  assume_role {
-    role_arn = "arn:aws:iam::905418146175:role/eks_admin_role"
-    session_name = "MySession"
-  }
+  
 }
 
 provider "kubernetes" {
   config_path = "/home/kug1be/.kube/config"
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
-      command     = "aws"
-    }
 }
 
 provider "helm" {
@@ -41,10 +33,5 @@ provider "helm" {
     config_path = "/home/kug1be/.kube/config"
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", var.eks_cluster_name]
-      command     = "aws"
-    }
   }
 }
